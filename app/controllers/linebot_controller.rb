4f07d386a5bd471a826edc8c18920d5114cce7ca
shoda888 <gekka888@gmail.com>
2019-08-10 18:04:33 +0900

    new3

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -39,35 +39,156 @@ class LinebotController < ApplicationController
   end
 
   def call
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-
-    # unless client.validate_signature(body, signature)
-    #   error 400 do 'Bad Request' end
-    # end
-    events = client.parse_events_from(body)
-    events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
-          # message = {
-          #   type: 'text',
-          #   text: '散歩開始'
-          # }
-          # pp event['source']['groupId']
-          # client.push_message('C5b56a06f5b1bd3c971785bf6e3f970cd'), message)
-          # client.push_message(event['source']['groupId'], message)
-        end
-      end
+    # pp '@@@@@@@@@@@@@@@@@@@@@@@@@'
+    # body = request.body.read
+    # pp body
+    message = {
+      type: 'text',
+      text: params[:temp]
     }
-    
+    client.push_message('C5b56a06f5b1bd3c971785bf6e3f970cd', message)
 
+    card = {
+      "type": "bubble",
+      "styles": {
+        "footer": {
+          "separator": true
+        }
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "今日の東京の天気",
+            "weight": "bold",
+            "color": "#1DB446",
+            "size": "md"
+          },
+          {
+            "type": "separator",
+            "margin": "xxl"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "margin": "xxl",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Description",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "$2.99",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Temp",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "$0.99",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "MaxTemp",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "$3.33",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "MinTemp",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "$3.33",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Humidity",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "$3.33",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    }
+    client.push_message('C5b56a06f5b1bd3c971785bf6e3f970cd', card)
+    # params[:price_jp]
+    # params[:price_ny]
+    # params[:description]
+    # params[:temp]
+    # params[:temp_min]
+    # params[:temp_max]
+    # params[:humidity]
     head :ok
   end
 end
