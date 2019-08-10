23197bd00f03b9aebe671964c92a72335069ac4c
shoda888 <gekka888@gmail.com>
2019-08-10 15:05:18 +0900

    new

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -15,12 +15,43 @@ class LinebotController < ApplicationController
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
 
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
+    # unless client.validate_signature(body, signature)
+    #   error 400 do 'Bad Request' end
+    # end
 
     events = client.parse_events_from(body)
 
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          # message = {
+          #   type: 'text',
+          #   text: event.message['text']
+          # }
+          # client.reply_message(event['replyToken'], message)
+          message = {
+            type: 'text',
+            text: '散歩開始'
+          }
+          pp event['source']['groupId']
+          client.push_message('C5b56a06f5b1bd3c971785bf6e3f970cd', message)
+        end
+      end
+    end
+
+    head :ok
+  end
+
+  def call
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+
+    # unless client.validate_signature(body, signature)
+    #   error 400 do 'Bad Request' end
+    # end
+    events = client.parse_events_from(body)
     events.each { |event|
       case event
       when Line::Bot::Event::Message
@@ -31,26 +62,17 @@ class LinebotController < ApplicationController
             text: event.message['text']
           }
           client.reply_message(event['replyToken'], message)
+          # message = {
+          #   type: 'text',
+          #   text: '散歩開始'
+          # }
+          # pp event['source']['groupId']
+          # client.push_message('C5b56a06f5b1bd3c971785bf6e3f970cd'), message)
+          # client.push_message(event['source']['groupId'], message)
         end
       end
     }
-
-    head :ok
-  end
-
-  def call
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
-
-    message = {
-      type: 'text',
-      text: '散歩開始'
-    }
-    client.push_message('C7cdceb50938ad9a23110491e911f3b51', message)
+    
 
     head :ok
   end
