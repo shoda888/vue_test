207ac323a2c1a8b67028bb431542be471a1d5266
shoda888 <gekka888@gmail.com>
2019-08-11 13:50:13 +0900

    oifhaseiofsioehf

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -69,39 +69,220 @@ class LinebotController < ApplicationController
     }
 
     card = {
-      "type": "bubble",
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "今日の東京の天気",
-            "weight": "bold",
-            "gravity": "center",
-            "size": "xl"
-          },
-          {
-            "type": "text",
-            "text": "#{params['desc']}"
-          },
-          {
-            "type": "text",
-            "text": "#{params['temp']}"
-          },
-          {
-            "type": "text",
-            "text": "#{params['max']}"
-          },
-          {
-            "type": "text",
-            "text": "#{params['min']}"
-          },
-          {
-            "type": "text",
-            "text": "#{params['humidity']}"
+      "type": "flex",
+      "altText": "this is a flex message",
+      "contents": {
+        "type": "bubble",
+        "styles": {
+          "footer": {
+            "separator": true
           }
-        ]
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "RECEIPT",
+              "weight": "bold",
+              "color": "#1DB446",
+              "size": "sm"
+            },
+            {
+              "type": "text",
+              "text": "Brown Store",
+              "weight": "bold",
+              "size": "xxl",
+              "margin": "md"
+            },
+            {
+              "type": "text",
+              "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+              "size": "xs",
+              "color": "#aaaaaa",
+              "wrap": true
+            },
+            {
+              "type": "separator",
+              "margin": "xxl"
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "margin": "xxl",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "Energy Drink",
+                      "size": "sm",
+                      "color": "#555555",
+                      "flex": 0
+                    },
+                    {
+                      "type": "text",
+                      "text": "$2.99",
+                      "size": "sm",
+                      "color": "#111111",
+                      "align": "end"
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "Chewing Gum",
+                      "size": "sm",
+                      "color": "#555555",
+                      "flex": 0
+                    },
+                    {
+                      "type": "text",
+                      "text": "$0.99",
+                      "size": "sm",
+                      "color": "#111111",
+                      "align": "end"
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "Bottled Water",
+                      "size": "sm",
+                      "color": "#555555",
+                      "flex": 0
+                    },
+                    {
+                      "type": "text",
+                      "text": "$3.33",
+                      "size": "sm",
+                      "color": "#111111",
+                      "align": "end"
+                    }
+                  ]
+                },
+                {
+                  "type": "separator",
+                  "margin": "xxl"
+                },
+                {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "margin": "xxl",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "ITEMS",
+                      "size": "sm",
+                      "color": "#555555"
+                    },
+                    {
+                      "type": "text",
+                      "text": "3",
+                      "size": "sm",
+                      "color": "#111111",
+                      "align": "end"
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "TOTAL",
+                      "size": "sm",
+                      "color": "#555555"
+                    },
+                    {
+                      "type": "text",
+                      "text": "$7.31",
+                      "size": "sm",
+                      "color": "#111111",
+                      "align": "end"
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "CASH",
+                      "size": "sm",
+                      "color": "#555555"
+                    },
+                    {
+                      "type": "text",
+                      "text": "$8.0",
+                      "size": "sm",
+                      "color": "#111111",
+                      "align": "end"
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "CHANGE",
+                      "size": "sm",
+                      "color": "#555555"
+                    },
+                    {
+                      "type": "text",
+                      "text": "$0.69",
+                      "size": "sm",
+                      "color": "#111111",
+                      "align": "end"
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              "type": "separator",
+              "margin": "xxl"
+            },
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "margin": "md",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "PAYMENT ID",
+                  "size": "xs",
+                  "color": "#aaaaaa",
+                  "flex": 0
+                },
+                {
+                  "type": "text",
+                  "text": "#743289384279",
+                  "color": "#aaaaaa",
+                  "size": "xs",
+                  "align": "end"
+                }
+              ]
+            }
+          ]
+        }
       }
     }
     card2 = {  
@@ -145,7 +326,7 @@ class LinebotController < ApplicationController
       }
     }
     
-    client.push_message('C5b56a06f5b1bd3c971785bf6e3f970cd', message)
+    client.push_message('C5b56a06f5b1bd3c971785bf6e3f970cd', card)
     client.push_message('C5b56a06f5b1bd3c971785bf6e3f970cd', card2)
     
     head :ok
